1cf5e3d58efa754f91380da6b7bce59be59d3a50
M.Sertac Turkel <msertacturkel@gmail.com>
2014-04-16 11:44:19 +0300

    Multi language

---
+++ MultiLanguage/src/com/example/activitymanagement/Activity_two.java
@@ -0,0 +1,49 @@
+package com.example.activitymanagement;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.os.Build;
+
+public class Activity_two extends ActionBarActivity {
+
+  TextView bilgi;
+  Button geri;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_two);
+    bilgi = (TextView) findViewById(R.id.bilgiMetni);
+    geri = (Button) findViewById(R.id.geriDugme);
+    String bilgiMetni = "";
+
+    if (getIntent().getExtras().getString(Sabitler.CINSIYET)
+        .equals(getString(R.string.text_erkek))) {
+      bilgiMetni = getString(R.string.text_bay);
+    } else if (getIntent().getExtras().getString(Sabitler.CINSIYET)
+        .equals(getString(R.string.text_kadin))) {
+      bilgiMetni = getString(R.string.text_bayan);
+    }
+
+    bilgiMetni += " " + getIntent().getExtras().getString(Sabitler.AD) + " "
+        + getIntent().getExtras().getString(Sabitler.SOYAD) + " ("
+        + getIntent().getExtras().getInt(Sabitler.YAS) + ") "
+        + getString(R.string.text_diyor)
+        + getIntent().getExtras().getString(Sabitler.ILETI);
+
+    bilgi.setText(bilgiMetni);
+  }
+
+  public void geri_tikla(View v) {
+    finish();
+  }
+}
---
+++ MultiLanguage/src/com/example/activitymanagement/Sabitler.java
@@ -0,0 +1,10 @@
+package com.example.activitymanagement;
+
+public class Sabitler {
+  public static final String AD = "ad";
+  public static final String SOYAD = "soyad";
+  public static final String YAS = "yas";
+  public static final String CINSIYET = "cinsiyet";
+  public static final String ILETI = "ileti";
+
+}
